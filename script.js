909b431cbd5f91d16bd851dc5772bba599e5b982
Joshua Winter <91838293+JaJa99335@users.noreply.github.com>
2023-12-16 12:17:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// ==UserScript==
+// @name         Anti Scam Checker - Advanced
+// @version      0.8
+// @description  Checks links on a website and looks for flags on websites
+// @author       Joshua Winter
+// @match        http*://*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_download
+// @grant        GM_notification
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/JaJa99335/Anti-Spam/main/script.js
+// @downloadURL  https://raw.githubusercontent.com/JaJa99335/Anti-Spam/main/script.js
+// ==/UserScript==
 
+// TBR (TO BE RELEASED)
